4bd7bbc1370e594418af64e21b12350c57c54f63
MarcosEduardo30 <marcos302k02@gmail.com>
2025-08-08 21:59:39 -0300

    feat: Implementado o gráfico de gastos por categoria ao resumo de gastos

--- src/app/resumo/Models/resumo.model.ts
+++ src/app/resumo/Models/resumo.model.ts
@@ -1,10 +1,16 @@
 export type resumo = {
     totalGastoMes: number,
     totalGastoAno: number,
-    gastosPorMes: GastoPorMes[]
+    gastosPorMes: GastoPorMes[],
+    gastosPorCategoria: GastoPorCategoria[]
 }
 
 type GastoPorMes = {
     mes: string,
+    valor: number
+}
+
+type GastoPorCategoria = {
+    categoria: string,
     valor: number
 }
--- src/app/resumo/resumo.component.ts
+++ src/app/resumo/resumo.component.ts
@@ -22,13 +22,18 @@ export class ResumoComponent implements OnInit {
   gastoPorMesLabel = "Gastos por mês"
 
   //Variaveis temporárias, esses dados virão a partir do resumo depois
-  categoriaData = [300, 100, 200]
-  categoriaLabels = ["Mercado", "Lazer", "Transporte"]
+  categoriaData: number[] = [];
+  categoriaLabels: string[] = [];
 
   ngOnInit(): void {
    const sub = this.resumoService.carregarResumo()
-    .subscribe(r => {
-        r.data.gastosPorMes.forEach(m => this.gastoPorMesData.push({x: m.mes,y:  m.valor}));
+    .subscribe(response => {
+        response.data.gastosPorMes.forEach(d => this.gastoPorMesData.push({x: d.mes,y:  d.valor}));
+        
+        response.data.gastosPorCategoria.forEach(d => {
+          this.categoriaData.push(d.valor)
+          this.categoriaLabels.push(d.categoria)
+        })
       })
 
   this.destroyRef.onDestroy(() => sub.unsubscribe());
